c218a1a10f4ae2c4bee5dd3450f7668a777e3c9d
parkjg20 <parkjg20@naver.com>
2022-07-29 18:02:01 +0900

    initial commit

---
+++ src/main/kotlin/com/neoguri/neogurinest/api/NeoguriNestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.neoguri.neogurinest.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeoguriNestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeoguriNestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/neoguri/neogurinest/api/NeoguriNestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.neoguri.neogurinest.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeoguriNestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
