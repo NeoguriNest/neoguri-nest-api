889adf355e37f29d3794851d435ed64de06c63cc
parkjg20 <parkjg20@naver.com>
2022-08-03 23:11:10 +0900

    health check controller and route added

---
+++ src/main/kotlin/com/neoguri/neogurinest/api/controller/HealthCheckController.kt
@@ -0,0 +1,18 @@
+package com.neoguri.neogurinest.api.controller;
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/health-check")
+@RestController
+class HealthCheckController {
+
+    @GetMapping("")
+    fun healthCheck(): ResponseEntity<String> {
+
+        return ResponseEntity.ok("I'm Healthy");
+    }
+
+}
