6496ec88de97989cf082b799c758d88d8d327bec
parkjg20 <parkjg20@naver.com>
2022-11-16 01:00:48 +0900

    NonNull 파라미터 Null인 경우 400 BAD_REQUEST 응답하도록 구현 # 21

--- src/main/kotlin/com/neoguri/neogurinest/api/presentation/error/NeoguriExceptionHandler.kt
+++ src/main/kotlin/com/neoguri/neogurinest/api/presentation/error/NeoguriExceptionHandler.kt
@@ -4,11 +4,10 @@ import com.neoguri.neogurinest.api.presentation.exception.HttpException
 import lombok.extern.slf4j.Slf4j
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.time.Instant
-
 
 @Slf4j
 @RestControllerAdvice
@@ -23,6 +22,21 @@ class NeoguriExceptionHandler {
         return ResponseEntity.status(exception.statusCode).body(
             ErrorResponseDto.of(
                 exception.statusCode,
+                exception.message
+            )
+        )
+    }
+
+    /**
+     * @desc RequestHandler 진입 시 NonNull 타입에서 NullPointerException 발생하는 경우 400 BAD_REQUEST 응답
+     */
+    @ExceptionHandler(NullPointerException::class)
+    fun handleNullPointer(exception: NullPointerException): ResponseEntity<ErrorResponseDto> {
+        logger.info("status code: {}\nstackTrace: {}", exception.message, exception.stackTrace)
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            ErrorResponseDto.of(
+                HttpStatus.BAD_REQUEST,
                 exception.message
             )
         )
