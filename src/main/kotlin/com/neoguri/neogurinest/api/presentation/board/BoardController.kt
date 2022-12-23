ce86f1efd5b9de7ff1e21d836c0e8394893e679e
parkjg20 <parkjg20@naver.com>
2022-12-23 15:06:52 +0900

    게시판 추가 및 상태 변경 API 등록 #35 #36

--- src/main/kotlin/com/neoguri/neogurinest/api/presentation/board/BoardController.kt
+++ src/main/kotlin/com/neoguri/neogurinest/api/presentation/board/BoardController.kt
@@ -1,4 +1,48 @@
 package com.neoguri.neogurinest.api.presentation.board
 
-class BoardController {
+import com.neoguri.neogurinest.api.application.board.dto.request.BoardAddDto
+import com.neoguri.neogurinest.api.application.board.dto.request.BoardStatusUpdateDto
+import com.neoguri.neogurinest.api.application.board.dto.response.BoardDto
+import com.neoguri.neogurinest.api.application.board.usecase.BoardAddUseCaseInterface
+import com.neoguri.neogurinest.api.application.board.usecase.BoardStatusUpdateUseCaseInterface
+import com.neoguri.neogurinest.api.domain.common.exception.DuplicatedEntityException
+import com.neoguri.neogurinest.api.presentation.BaseController
+import com.neoguri.neogurinest.api.presentation.exception.BadRequestException
+import com.neoguri.neogurinest.api.presentation.exception.ConflictException
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.net.URI
+
+@RestController
+@RequestMapping("/api/boards")
+class BoardController(
+    val add: BoardAddUseCaseInterface,
+    val updateStatus: BoardStatusUpdateUseCaseInterface
+) : BaseController() {
+
+    @PostMapping("")
+    fun add(@RequestBody boardAddDto: BoardAddDto): ResponseEntity<BoardDto> {
+        if (boardAddDto.title.trim().isEmpty()) {
+            throw BadRequestException("Body/title must be has length over 1")
+        }
+
+        return try {
+            val board = add.execute(boardAddDto)
+            ResponseEntity
+                .created(URI("/api/boards/${board.boardId}"))
+                .body(board)
+        } catch (e: DuplicatedEntityException) {
+            throw ConflictException(e.message!!)
+        }
+    }
+
+    @PutMapping("/status")
+    fun updateStatus(@RequestBody boardStatusUpdateDto: BoardStatusUpdateDto): ResponseEntity<BoardDto> {
+
+        return try {
+            ResponseEntity.ok(updateStatus.execute(boardStatusUpdateDto))
+        } catch (e: DuplicatedEntityException) {
+            throw ConflictException(e.message!!)
+        }
+    }
 }
